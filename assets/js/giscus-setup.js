466e63d21c59b3c87306209e934a642f80c718b6
YouranXia <fakemanxia@outlook.com>
2026-07-21 05:35:03 +0000

    Deploying to gh-pages from @ YouranXia/youranxia.github.io@9825eaab25fd4f9834f07874fcbfceaa3cf51ae2 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "fakemanxia/xiayouran07.github.io",
+    "data-repo": "YouranXia/youranxia.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
